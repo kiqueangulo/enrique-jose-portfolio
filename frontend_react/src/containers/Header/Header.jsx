8b6c003166391852fb42d25d61c467a270610bb8
kiqueangulo <kiqueangulo@gmail.com>
2022-08-15 22:31:56 -0400

    Adjust title

--- frontend_react/src/containers/Header/Header.jsx
+++ frontend_react/src/containers/Header/Header.jsx
@@ -34,8 +34,8 @@ const Header = () => {
           </div>
 
           <div className="tag-cmp app__flex">
-            <p className="p-text">Web Developer</p>
-            <p className="p-text">Freelancer</p>
+            <p className="p-text">Junior Software</p>
+            <p className="p-text">Developer</p>
           </div>
         </div>
       </motion.div>
